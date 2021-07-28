a3f416ae150d41bcf13b08df65f06d7de0b4d9ca
danielcr0995 <daniel.correa.1995@gmail.com>
2021-07-28 13:59:29 -0500

    chages to button pressing

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const btns=document.querySelectorAll('button')
+const btns=document.querySelectorAll('button');
+let k7=document.getElementById("7");
+k7.addEventListener('keydown',function(e){
+    console.log(e);
+})
 console.log(btns);
-btns.forEach(btn=>btn.addEventListener('keydown',function(e){
-    console.log(e)
+btns.forEach(btn=>btn.addEventListener('click',function(e){
+    console.log(e.target)
 }))
-window.addEventListener('click', function(e){
-    console.log(e.target.value);
-});
+// window.addEventListener('click', function(e){
+//     console.log(e.target.value);
+// });
 
 window.addEventListener("keydown", function(e){
-    console.log(e);
+    console.log(e.value);
 });
